d9231608749d1c29faec3495eac0618d88f10628
pixel4lex <pixel4lex@gmail.com>
2025-06-17 17:04:17 +0300

    still fixing bugs

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers.auth import router as auth_router
-from routers.game import router as game_router
+from .routers.auth import router as auth_router
+from .routers.game import router as game_router
 
 app = FastAPI()
 
@@ -19,4 +19,4 @@ app.include_router(game_router, prefix="/api/game", tags=["game"])
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("backend.app.main:app", host="127.0.0.1", port=8000, reload=True)
