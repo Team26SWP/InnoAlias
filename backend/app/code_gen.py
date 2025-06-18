5ae7574726b10132a96de2eeeda52da753d9cbcf
pixel4lex <pixel4lex@gmail.com>
2025-06-18 17:02:25 +0300

    fully working game codes

--- backend/app/code_gen.py
+++ backend/app/code_gen.py
@@ -4,6 +4,6 @@ from backend.app.db import db
 
 async def generate_code():
     while True:
-        code = ''.join(choice(ascii_uppercase + digits) for _ in range(6))
-        if not await db.games.find_one({"_id": id}):
+        code = "".join(choice(ascii_uppercase + digits) for _ in range(6))
+        if not await db.games.find_one({"_id": code}):
             return code
