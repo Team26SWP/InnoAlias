7efe262afb06162e0894e4a6cdf84e63a6e38e2b
pixel4lex <pixel4lex@gmail.com>
2025-06-18 23:35:37 +0300

    buggy version of differen masters

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -13,6 +13,8 @@ class Game(BaseModel):
     time_for_guessing: int = 60
     tries_per_player: int = 0
     right_answers_to_advance: int = 1
+    rotate_masters: bool = False
+    current_master: Optional[str] = None
     correct_players: List[str] = Field(default_factory=list)
     state: Literal["pending", "in_progress", "finished"] = "pending"
     scores: Dict[str, int] = Field(default_factory=dict)
@@ -24,6 +26,7 @@ class GameState(BaseModel):
     remaining_words_count: int
     current_correct: int = 0
     right_answers_to_advance: int = 1
+    current_master: Optional[str] = None
     state: Literal["pending", "in_progress", "finished"]
     scores: Dict[str, int] = Field(default_factory=dict)
 
@@ -32,6 +35,8 @@ class PlayerGameState(BaseModel):
     expires_at: Optional[datetime]
     remaining_words_count: int
     tries_left: Optional[int] = None
+    current_word: Optional[str] = None
+    current_master: Optional[str] = None
     state: Literal["pending", "in_progress", "finished"]
     scores: Dict[str, int] = Field(default_factory=dict)
 
