08e4f50916bfc7acd5e9df8827e87b694e7a454b
Aleksei Kovalenko <pixel4lex@gmail.com>
2025-06-27 14:19:52 +0300

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,6 @@
 from os import getenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = getenv("MONGO_URI", "mongodb://mongo:27017/")
+MONGO_URI = getenv("MONGO_URI", "mongodb://localhost:27017/")
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.db
