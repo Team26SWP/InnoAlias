c34d83fe2d8eb1c297ad6bda13b215beb0f0e277
shokkazz <foxy92921@gmail.com>
2025-07-04 17:26:20 +0300

    DB cursors reusage fix

--- backend/app/routers/gallery.py
+++ backend/app/routers/gallery.py
@@ -22,19 +22,21 @@ async def get_gallery(number: int):
 
 @router.put("/decks/save/{deck_id}")
 async def save_deck_from_gallery (
-    deck: Deck,
+    deck_id: str,
     current_user: UserInDB = Depends(get_current_user),
 ):
-    if not await users.find_one({"_id": current_user.id}):
+    temp_user = await users.find_one({"_id": current_user.id}) if current_user else None
+    temp_deck = await decks.find_one({"_id": deck_id}) if deck_id else None
+    if not await temp_user:
         raise HTTPException(status_code=404, detail="User not found")
-    if not await decks.find_one({"_id": deck.id}):
+    if not await temp_deck:
         raise HTTPException(status_code=404, detail="Deck not found")
-    if current_user.id in await decks.find_one({"_id": deck.id}).get("owner_ids"):
+    if current_user.id in await temp_deck.get("owner_ids"):
         raise HTTPException(status_code=404, detail="Already have this deck")
-    if await decks.find_one({"_id": deck.id}).get("private"):
+    if await temp_deck.get("private"):
         raise HTTPException(status_code=403, detail="Forbidden")
-    await decks.update_one({"_id": deck.id}, {"$addToSet": {"owner_ids": current_user.id}})
+    await decks.update_one({"_id": deck_id}, {"$addToSet": {"owner_ids": current_user.id}})
     await users.update_one(
-        {"_id": current_user.id}, {"$addToSet": {"deck_ids": deck.id}}
+        {"_id": current_user.id}, {"$addToSet": {"deck_ids": deck_id}}
 )
-    return {"saved_deck_id": deck.id}
+    return {"saved_deck_id": deck_id}
