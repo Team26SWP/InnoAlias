e0cb023ff0d800654316af510d1cbf38d7ebf721
DeBb1Kk <misterloh228337@gmail.com>
2025-07-05 17:03:14 +0300

    fixed button

--- frontend/src/components/config.ts
+++ frontend/src/components/config.ts
@@ -114,7 +114,7 @@ const creationState: GameCreationState = {
   words: [],
 };
 
-const HOST = window.location.hostname;
+const HOST = 'localhost:8000';
 export const WS_URL = `ws://${HOST}/api`;
 export const HTTP_URL = `http://${HOST}/api`;
 
