93f58e2c396fca039822bfc7b8e9972b180f234f
Falbne <albertars021@gmail.com>
2025-07-13 15:53:18 +0300

    black fix

--- backend/app/routers/admin_panel.py
+++ backend/app/routers/admin_panel.py
@@ -44,7 +44,7 @@ async def delete_user(
 async def get_logs(current_user=Depends(get_current_user)):
     if not current_user.isAdmin:
         raise HTTPException(status_code=403, detail="Forbidden")
-    #change fix do smth    
+    # change fix do smth
     show_logs = await logs.find().to_list(length=None)
     if not show_logs:
         raise HTTPException(status_code=404, detail="No logs")
