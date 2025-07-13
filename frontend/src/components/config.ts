393177bfe6a6567ebd9e2750faae278b47bd3acb
Falbne <albertars021@gmail.com>
2025-07-13 20:30:57 +0300

    almost final adminPanel fixes

--- frontend/src/components/config.ts
+++ frontend/src/components/config.ts
@@ -143,7 +143,8 @@ const creationState: GameCreationState = {
   aiGame: false,
 };
 
-const HOST = window.location.hostname;
+// const HOST = window.location.hostname;
+const HOST = 'localhost:8000';
 export const WS_URL = `ws://${HOST}/api`;
 export const HTTP_URL = `http://${HOST}/api`;
 
