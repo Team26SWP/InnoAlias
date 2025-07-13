6ea845a20bbae423d058955cea28f50fda413738
Falbne <albertars021@gmail.com>
2025-07-13 20:33:00 +0300

    .

--- frontend/src/components/config.ts
+++ frontend/src/components/config.ts
@@ -143,8 +143,8 @@ const creationState: GameCreationState = {
   aiGame: false,
 };
 
-// const HOST = window.location.hostname;
-const HOST = 'localhost:8000';
+const HOST = window.location.hostname;
+// const HOST = 'localhost:8000';
 export const WS_URL = `ws://${HOST}/api`;
 export const HTTP_URL = `http://${HOST}/api`;
 
